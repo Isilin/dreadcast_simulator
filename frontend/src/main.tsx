c7d437c7547cb8d60bf357d9fd27461a73dd26bc
IsilinBN <casa2pir@hotmail.fr>
2025-10-25 17:22:08 +0200

    Moves metadata to index.html
    Transfers metadata from the main React component to index.html.
    
    This improves SEO and allows crawlers to access the
    information without executing JavaScript.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -29,46 +29,6 @@ if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement);
   root.render(
     <StrictMode>
-      <title>DreadCast Simulator</title>
-      <meta
-        name="description"
-        content="Compose ton build, compare les stats et partage un lien."
-      />
-
-      <meta
-        property="og:url"
-        content="https://dreadcast-simulator-kappa.vercel.app/"
-      />
-      <meta property="og:type" content="website" />
-      <meta property="og:title" content="Construis ton équipement" />
-      <meta
-        property="og:description"
-        content="Compose ton build, compare les stats et partage un lien."
-      />
-      <meta
-        property="og:image"
-        content="https://opengraph.b-cdn.net/production/images/849b1e80-62a3-4848-b661-99984376e3ef.png?token=ir-CtzjHBYyyyNNCrkZjJe3MO5K2SfWhv9HaZ8Fu7YY&height=656&width=1200&expires=33297405378"
-      />
-
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta
-        property="twitter:domain"
-        content="dreadcast-simulator-kappa.vercel.app"
-      />
-      <meta
-        property="twitter:url"
-        content="https://dreadcast-simulator-kappa.vercel.app/"
-      />
-      <meta name="twitter:title" content="Construis ton équipement" />
-      <meta
-        name="twitter:description"
-        content="Compose ton build, compare les stats et partage un lien."
-      />
-      <meta
-        name="twitter:image"
-        content="https://opengraph.b-cdn.net/production/images/849b1e80-62a3-4848-b661-99984376e3ef.png?token=ir-CtzjHBYyyyNNCrkZjJe3MO5K2SfWhv9HaZ8Fu7YY&height=656&width=1200&expires=33297405378"
-      />
-
       <QueryClientProvider client={queryClient}>
         <Tooltip.Provider>
           <ImplantsProvider>
