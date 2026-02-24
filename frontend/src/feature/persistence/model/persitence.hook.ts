3c777ee3de0156e8bfee8a0ba29e89158c45206a
IsilinBN <casa2pir@hotmail.fr>
2026-02-24 20:46:50 +0100

    fix(persistence): reset to empty slots if no data saved.

--- frontend/src/feature/persistence/model/persitence.hook.ts
+++ frontend/src/feature/persistence/model/persitence.hook.ts
@@ -8,9 +8,13 @@ import {
 } from '../services/persistence.service';
 
 import type { ImplantsState } from '@/feature/implant';
+import { initialState as implantsInitialState } from '@/feature/implant/model/implant.actions';
 import type { ItemsState } from '@/feature/item';
+import { initialState as itemsInitialState } from '@/feature/item/model/item.actions';
 import type { KitsState } from '@/feature/kit';
+import { initialState as kitsInitialState } from '@/feature/kit/model/kit.actions';
 import type { ProfileState } from '@/feature/profile';
+import { initialState as profileInitialState } from '@/feature/profile/model/profile.actions';
 
 interface HookParams {
   profile: ProfileState;
@@ -57,6 +61,12 @@ export function useBuildPersistence({
       implantsDispatch.replaceImplants(data.implants as ImplantsState);
       itemsDispatch.replaceItems(data.items as ItemsState);
       kitsDispatch.replaceKits(data.kits as KitsState);
+    } else {
+      // Reset to initial state for empty build slots
+      profileDispatch.replaceProfile(profileInitialState);
+      implantsDispatch.replaceImplants(implantsInitialState);
+      itemsDispatch.replaceItems(itemsInitialState);
+      kitsDispatch.replaceKits(kitsInitialState);
     }
     setTimeout(() => {
       isRestoringRef.current = false;
